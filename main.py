fbd8cfbd280493631edd4e8738f12ae96aee6a9c
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-01-27 23:52:11 +0700

    test ada

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import sys
 from Adafruit_IO import MQTTClient
+
+AIO_FEED_IDs = ["button1" , "button2"]
+AIO_USERNAME = "PDucMinh"
+AIO_KEY = "aio_oUaW58kVAHOecHG1pTg1YzNLiY20"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
+    
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " feed id: " + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+while True:
+    pass
